3a492308668514c278ab549cc051fb0eb8df2dfd
Clayton Wakefield <acidduckling@gmail.com>
2018-04-06 19:59:28 +1000

    Photo upload

--- Helpers/AutoMapperProfile.cs
+++ Helpers/AutoMapperProfile.cs
@@ -28,6 +28,9 @@ namespace DatingApp.API.Helpers
             CreateMap<Photo, PhotosForDetailDto>();
 
             CreateMap<UserForUpdateDto, User>();
+
+            CreateMap<PhotoForCreationDto, Photo>();
+            CreateMap<Photo, PhotoForReturnDto>();
         }
     }
 }
