3c85c8c7124e1768351b54e071f28ecb6feeaf31
Clayton Wakefield <acidduckling@gmail.com>
2018-04-07 10:38:53 +1000

    Register form

--- Helpers/AutoMapperProfile.cs
+++ Helpers/AutoMapperProfile.cs
@@ -26,11 +26,10 @@ namespace DatingApp.API.Helpers
                 });
 
             CreateMap<Photo, PhotosForDetailDto>();
-
             CreateMap<UserForUpdateDto, User>();
-
             CreateMap<PhotoForCreationDto, Photo>();
             CreateMap<Photo, PhotoForReturnDto>();
+            CreateMap<UserForRegisterDto, User>();
         }
     }
 }
